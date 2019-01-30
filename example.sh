576dde3a9a66282e6612ea66505fb1f75921a6c7
Alexander Alemayhu <alexander@alemayhu.com>
2019-01-30 23:40:42 +0100

    wip load from file
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

---
+++ example.sh
@@ -0,0 +1,17 @@
+# vi:syntax=sh
+
+export VISUAL=vim
+
+# Locale
+export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
+export GOROOT=~/opt/go
+export PATH=$PATH:$GOROOT/bin
+export PATH=$PATH:~/Scripts
+export GOPATH="$HOME"/opt/og
+export PATH=$PATH:$GOPATH/bin
+export PATH=~/.local/bin:$PATH
+export PATH=$HOME/src/llvm.org/llvm/build/bin:$PATH
+export PATH=/Applications/Wine\ Stable.app/Contents/Resources/wine/bin:$PATH
--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 import test from 'ava';
 
-import { fromString } from './lib/read-path';
+import { fromString, fromFile } from './lib/read-path';
 
 
 test('single from string', t => {
@@ -12,5 +12,12 @@ test('single from string', t => {
 test('multiple from string', t => {
   const expected = ['/usr/local/bin', '/usr/bin'];
   const actual = fromString("PATH=/usr/local/bin\nPATH=$PATH:/usr/bin");
+  t.deepEqual(expected, actual);
+});
+
+test('multiple from file', t => {
+  const expected = [
+  ];
+  const actual = fromFile("example.sh", "utf-8");
   t.deepEqual(expected, actual);
 });
